19e58c50c005da7d3bf7b10b3cd6b2d02df42120
mspena14 <marlono1naranjo@gmail.com>
2024-08-20 16:52:39 -0500

    Avanzado en clase 20/08

--- Semana02/ecommerce-nest-js/src/app.module.ts
+++ Semana02/ecommerce-nest-js/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common/common.module';
+
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, OrdersModule, AuthModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
