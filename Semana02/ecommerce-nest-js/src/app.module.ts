dfc8855401fed9c1e4488d80ecc073d705a25fa7
mspena14 <marlono1naranjo@gmail.com>
2024-08-21 16:36:17 -0500

    final clase 21/08

--- Semana02/ecommerce-nest-js/src/app.module.ts
+++ Semana02/ecommerce-nest-js/src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common/common.module';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
